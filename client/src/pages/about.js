1a583d4dec381a4808cfd146533a2eb0d2115b6a
Idilhyder <idilhyder@gmail.com>
2020-03-21 01:53:54 -0400

    done for t-4 days

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { motion } from "framer-motion";
-import "./about.scss";
+import * as React from "react";
+import './about.scss';
 
-function About ()  {
-    return (
-        <div className="pandemic">
-            <h1> MY NAME IS IDIL AND THIS APP IS BROUGHT TO YOU THROUGH THE FIRST PANDEMIC
-                OF MY LIFETIME. ENJOY AND DON'T WASTE FOOD
-            </h1>
-        </div>
+
+const About = () => {
+    return(
+        <div class="text">
+        <div class="r">MY NAME IS IDIL AND THIS APP IS BROUGHT TO YOU THROUGH THE FIRST PANDEMIC
+        OF MY LIFETIME. ENJOY AND DON'T WASTE FOOD</div>
+        <div class="g">MY NAME IS IDIL AND THIS APP IS BROUGHT TO YOU THROUGH THE FIRST PANDEMIC
+        OF MY LIFETIME. ENJOY AND DON'T WASTE FOOD</div>
+        <div class="b">PANDEMIC</div>
+     </div>
     )
-}
+
+
+};
+
 export default About;
+
